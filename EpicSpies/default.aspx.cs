0242ed162838aa68dffcf941b7228bdbadc64772
Vladislav Golovin <golovin2011@gmail.com>
2016-09-02 17:54:37 +0300

    Some code optimization + added comments.

--- EpicSpies/default.aspx.cs
+++ EpicSpies/default.aspx.cs
@@ -11,6 +11,7 @@ namespace EpicSpies
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //The initial calendar setup + calendar behvior after the page reload
             Calendar1.SelectedDate = DateTime.Now.Date;
             if (!Page.IsPostBack)
             {
@@ -21,25 +22,25 @@ namespace EpicSpies
 
         protected void resultButton_Click(object sender, EventArgs e)
         {
+            //Variable
             string spyName = spyNameTextBox.Text;
             string spyAssignment = spyAssignmentTextBox.Text;
+            double budget = ((Calendar3.SelectedDate.DayOfYear - Calendar2.SelectedDate.DayOfYear) * 500);
+            double extraBudget = ((Calendar3.SelectedDate.DayOfYear - Calendar2.SelectedDate.DayOfYear) * 500 + 1000);
 
+            //Logic
             if ((Calendar1.SelectedDate.DayOfYear + 14) > Calendar2.SelectedDate.DayOfYear)
             {
-                resultLabel.Text = "Must allow at least 2 weeks between the previous assigment and new assignment!".ToUpper();
+                resultLabel.Text = "Must allow at least 2 weeks between the previous assigment and new assignment!";
                 Calendar2.SelectedDate = DateTime.Now.Date.AddDays(14);
             }
             else if ((Calendar3.SelectedDate.DayOfYear - Calendar2.SelectedDate.DayOfYear) > 21)
             {
-                double budget =
-                    ((Calendar3.SelectedDate.DayOfYear - Calendar2.SelectedDate.DayOfYear)*500 + 1000);
                 resultLabel.Text = String.Format("Assignment of {0} to assignment {1} is authorized. " +
-                                                 "Budget total: {2:C}", spyName, spyAssignment, budget);
+                                                 "Budget total: {2:C}", spyName, spyAssignment, extraBudget);
             }
             else
             {
-                double budget =
-                    ((Calendar3.SelectedDate.DayOfYear - Calendar2.SelectedDate.DayOfYear) * 500);
                 resultLabel.Text = String.Format("Assignment of {0} to assignment '{1}' is authorized. " +
                                                  "Budget total: {2:C}", spyName, spyAssignment, budget);
             }
